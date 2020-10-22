5f5646ecfdc8f91e08beee9fdc8f1a5631d160bf
eXConf <sergey.sosedkin@gmail.com>
2020-10-22 09:31:56 +0300

    [ref] code blocks order

--- src/components/SummaryBlock.js
+++ src/components/SummaryBlock.js
@@ -4,56 +4,6 @@ import { copyToClipboard } from './helpers'
 
 class SummaryBlock extends React.Component {
 
-  renderPlayerResults = (index) => {
-    const results = this.calcPlayerResults(index)
-    if (!results) {
-      return <>0 <br />[+0/-0]</>
-    }
-    const {sum, correct, incorrect} = results
-    return (
-      <>{sum} <br />
-      {`[+${correct}/-${incorrect}]`}</>
-    )
-  }
-
-  calcPlayerResults = (index) => {
-    if (!this.props.players[index]) return null
-    const scores = this.props.players[index].answers
-    if (scores.length === 0) return null
-    const sum = scores.reduce((acc, current) => acc + current)
-    const correct = scores.filter((score) => score > 0).length
-    const incorrect = scores.filter((score) => score < 0).length
-    return {sum, correct, incorrect}
-  }
-
-  getAllPlayersResults = () => {
-    const allResults = []
-    console.log(this.props.players.length)
-    this.props.players.forEach((el, index) => {
-      const results = this.calcPlayerResults(index)
-      const name = this.props.players[index].name
-      if (!results) {
-        allResults.push(`${name}: 0 [+0/-0]`)
-      } else {
-        const {sum, correct, incorrect} = results
-        allResults.push(`${name}: ${sum} [+${correct}/-${incorrect}]`)
-      }
-    });
-    return allResults
-  }
-  
-  renderPlayerSum = () => {
-    return [...Array(this.props.numberOfPlayers)].map(
-      (el, index) => {
-        return (
-          <td key={`p${index}`} className="player-sum">
-            {this.renderPlayerResults(index)}
-          </td>
-        )
-      }
-    )
-  }
-
   render() {
     return (
      <React.Fragment>
@@ -73,6 +23,55 @@ class SummaryBlock extends React.Component {
         </tr> 
      </React.Fragment>
     )
+  }
+
+  renderPlayerResults = (index) => {
+    const results = this.calcPlayerResults(index)
+    if (!results) {
+      return <>0 <br />[+0/-0]</>
+    }
+    const {sum, correct, incorrect} = results
+    return (
+      <>{sum} <br />
+      {`[+${correct}/-${incorrect}]`}</>
+    )
+  }
+
+  renderPlayerSum = () => {
+    return [...Array(this.props.numberOfPlayers)].map(
+      (el, index) => {
+        return (
+          <td key={`p${index}`} className="player-sum">
+            {this.renderPlayerResults(index)}
+          </td>
+        )
+      }
+    )
+  }
+
+  calcPlayerResults = (index) => {
+    if (!this.props.players[index]) return null
+    const scores = this.props.players[index].answers
+    if (scores.length === 0) return null
+    const sum = scores.reduce((acc, current) => acc + current)
+    const correct = scores.filter((score) => score > 0).length
+    const incorrect = scores.filter((score) => score < 0).length
+    return {sum, correct, incorrect}
+  }
+
+  getAllPlayersResults = () => {
+    const allResults = []
+    this.props.players.forEach((el, index) => {
+      const results = this.calcPlayerResults(index)
+      const name = this.props.players[index].name
+      if (!results) {
+        allResults.push(`${name}: 0 [+0/-0]`)
+      } else {
+        const {sum, correct, incorrect} = results
+        allResults.push(`${name}: ${sum} [+${correct}/-${incorrect}]`)
+      }
+    });
+    return allResults
   }
 }
 
